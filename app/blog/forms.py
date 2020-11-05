4cbbb652e618ec2895261c6b1875695cb7d956dd
Adam <adam.makowski@mentoro.online>
2020-11-05 13:55:51 +0100

    add lessons to courses

--- app/blog/forms.py
+++ app/blog/forms.py
@@ -4,7 +4,7 @@ from .models import Post, Comment
 class BlogForm(forms.ModelForm):
     class Meta:
         model = Post
-        fields = ('title', 'category', 'language', 'content', 'image_preview', 'status')
+        fields = ('title', 'category', 'language', 'short_description', 'content', 'image_preview', 'status')
 
 class CommentForm(forms.ModelForm):
     class Meta:
